8e30a6c4933c975672d317970e46b251a98cbbcd
Maclay <mishakozlov74@gmail.com>
2019-10-26 08:37:54 +0300

    remove old code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import Text from "./new";
 
-Object.assign(PIXI.extras, {
-  AwesomeText: Text
-});
-
-if (typeof PIXI !== 'undefined') {
-  if (PIXI.extras) {
-    PIXI.extras.AwesomeText = Text;
-  } else {
-    PIXI.extras = { AwesomeText: Text};
-  }
-}
-
 export {
   Text
 }
